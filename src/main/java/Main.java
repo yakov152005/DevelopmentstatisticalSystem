a602b192bcdea5692ef61923474a32a77bf0f760
yakov152005 <yakov152005@gmail.com>
2024-06-20 23:30:02 +0300

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,6 +11,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
         CountryStatisticsManager csm = new CountryStatisticsManager();
         LinkedList<Country> countryList = new LinkedList<>();
         csm.loadDataFromFile(countryList, "/data.csv");
@@ -26,8 +27,10 @@ public class Main {
                 System.out.println("Invalid option. Try again.");
                 continue;
             }
+
 //Name,Population,GDP,enums.Continent,EconomicRating,PopulationRating,ContinentRating
             switch (option) {
+
                 case ADD_COUNTRY:
                     System.out.println("Enter Name of country: ");
                     String name = scanner.nextLine();
@@ -53,124 +56,90 @@ public class Main {
                 case FIND_COUNTRY:
                     System.out.println("Search country: ");
                     String search = scanner.nextLine();
-                    for (int i = 0; i < countryList.size(); i++) {
-                        if (countryList.get(i).getName().equalsIgnoreCase(search)) {
-                            System.out.println(countryList.get(i));
-                        }
-                    }
+                    findCountry(search,countryList);
                     break;
+
                 case LIST_COUNTRIES:
                     System.out.println("All country: ");
                     for (Country country1 : countryList) {
                         System.out.println(country1);
                     }
                     break;
-                case SORT_COUNTRIES:
-                    List<Country> filteredCountries = null;
 
+                case SORT_COUNTRIES:
                     System.out.println("Filter countries by continent\n" +
                             "Choose which continent you would like to filter by -- >");
-
                     int index = 1;
                     for (Enum e: Continent.values()){
                         System.out.println(index++ + " - " + e);
                     }
+                    
                     //AFRICA, ANTARCTICA, ASIA, EUROPE, NORTH_AMERICA, AUSTRALIA, SOUTH_AMERICA;
-
                     String selectStr = scanner.nextLine();
-                    String land = "";
-                    int select = Integer.parseInt(selectStr);
-                    switch (select){
-                        case 1:
-                            land = "AFRICA";
-                            filteredCountries =newFilterList(land,countryList);
-                            break;
-                        case 2:
-                            land = "ANTARCTICA";
-                            filteredCountries =newFilterList(land,countryList);
-                            break;
-                        case 3:
-                            land = "ASIA";
-                            filteredCountries =newFilterList(land,countryList);
-                            break;
-                        case 4:
-                            land = "EUROPE";
-                            filteredCountries =newFilterList(land,countryList);
-                            break;
-                        case 5:
-                            land = "NORTH_AMERICA";
-                            filteredCountries =newFilterList(land,countryList);
-                            break;
-                        case 6:
-                            land = "AUSTRALIA";
-                            filteredCountries =newFilterList(land,countryList);
-                            break;
-                        case 7:
-                            land = "SOUTH_AMERICA";
-                            filteredCountries =newFilterList(land,countryList);
-                            break;
-                        default:
-                            System.out.println("Invalid number.");
-                    }
-                    assert filteredCountries != null;
-                    if (!filteredCountries.isEmpty()) {
-                        for (Country c : filteredCountries) {
-                            System.out.println(c);
-                        }
-                    }else {
-                        System.out.println("Filtering failed.");
-                    }
-
+                    sortedCountryByMainLand(selectStr,countryList);
+                    
                     break;
+
                 case CONTINENT_FILTER:
                     menuForSwitchSorted();
                     String choiceRes = scanner.nextLine();
                     int choice = Integer.parseInt(choiceRes);
                     switchSortedList(countryList,choice);
                     break;
+
                 case QUIT:
                     System.out.println("Exiting...");
                     break;
+
+                default:
+                    throw new IllegalStateException("Unexpected value: " + option);
             }
 
         } while (option != MenuOption.QUIT);
 
         scanner.close();
     }
+
     public static void switchSortedList(LinkedList<Country> countryList, int choice){
-        while (true){
-            if (choice == 0){
-                break;
-            }
-            switch (choice){
+        if (choice ==0){
+            System.out.println("Back to the menu...");
+        }
+        while (choice != 0) {
+            switch (choice) {
                 case 1:
                     countryList.sort(new Comparators.CompareCountryByName());
                     choice = 0;
+                    System.out.println("the Sorted success.");
                     break;
                 case 2:
                     countryList.sort(new Comparators.CompareCountryByPopulation());
                     choice = 0;
+                    System.out.println("the Sorted success.");
                     break;
                 case 3:
                     countryList.sort(new Comparators.CompareCountryByGdpLevel());
                     choice = 0;
+                    System.out.println("the Sorted success.");
                     break;
                 case 4:
                     countryList.sort(new Comparators.CompareCountryByMainland());
                     choice = 0;
+                    System.out.println("the Sorted success.");
                     break;
                 case 5:
                     menuForSwitchedSortedByRating();
                     int select = s.nextInt();
-                    switchSortedByRating(countryList,select);
+                    switchSortedByRating(countryList, select);
+                    System.out.println("the Sorted success.");
                     choice = 0;
                     break;
                 default:
                     System.out.println("You entered an incorrect number, try again");
-                    break;
+                    switchSortedList(countryList, s.nextInt());
+                    return;
             }
         }
-        System.out.println("the Sorted success.");
+
 
     }
     public static void menuForSwitchSorted(){
@@ -225,7 +194,75 @@ public class Main {
         }
         return filteredCountries;
     }
-    public static Scanner s = new Scanner(System.in);
 
+    public static void findCountry(String search,LinkedList<Country> countryList){
+        search = search.toUpperCase();
+
+        for (int i = 0; i < countryList.size(); i++) {
+            if (countryList.get(i).getName().equalsIgnoreCase(search)) {
+                System.out.println(countryList.get(i));
+            } else if (countryList.get(i).getName().length() > search.length()) {
+                if (countryList.get(i).getName().charAt(0) == search.charAt(0)){
+                    System.out.println(countryList.get(i));
+                }
+            }
+        }
+    }
+    public static void sortedCountryByMainLand(String selectStr, LinkedList<Country> countryList){
+        List<Country> filteredCountries = null;
+
+        String land = "";
+        int select = 0;
+        try {
+            select = Integer.parseInt(selectStr);
+        } catch (NumberFormatException e) {
+
+        }
+        switch (select){
+            case 1:
+                land = "AFRICA";
+                filteredCountries = newFilterList(land,countryList);
+                break;
+            case 2:
+                land = "ANTARCTICA";
+                filteredCountries =newFilterList(land,countryList);
+                break;
+            case 3:
+                land = "ASIA";
+                filteredCountries =newFilterList(land,countryList);
+                break;
+            case 4:
+                land = "EUROPE";
+                filteredCountries =newFilterList(land,countryList);
+                break;
+            case 5:
+                land = "NORTH_AMERICA";
+                filteredCountries =newFilterList(land,countryList);
+                break;
+            case 6:
+                land = "AUSTRALIA";
+                filteredCountries =newFilterList(land,countryList);
+                break;
+            case 7:
+                land = "SOUTH_AMERICA";
+                filteredCountries =newFilterList(land,countryList);
+                break;
+            default:
+                System.out.println("Invalid Input.");
+        }
+        try {
+            assert filteredCountries != null;
+            if (!filteredCountries.isEmpty()) {
+                for (Country c : filteredCountries) {
+                    System.out.println(c);
+                }
+            }else {
+                System.out.println("Filtering failed.");
+            }
+        } catch (Exception e) {
+        }
+    }
+
+    public static Scanner s = new Scanner(System.in);
 }
 
